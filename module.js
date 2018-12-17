031d85625b31ea32138c863cfd3401efb7e68ec0
Sam Boylett <sam.boylett@netsells.co.uk>
2018-12-17 11:19:05 +0000

    Various fixes for being module based rather than running in same folder

--- module.js
+++ module.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path =require('path');
 var childProcess = require('child_process');
 var phantomjs = require('phantomjs/lib/phantomjs');
-var binPath = phantomjs.path
+var binPath = phantomjs.path;
 
 function evaluate(options) {
     var engine  = options.engine;      //Eg htmlcs, chrome, axe        default:htmlcs
@@ -10,7 +10,7 @@ function evaluate(options) {
     var level = options.level;         //E.g. WCAG2AA, WCAG2A, WCAG2AAA, Section508    default:WCAG2AA
     var errLevel = options.errLevel;   // Eg. 1,2,3   1 means Error, 2 means Warning, 3 means Notice   default:1,2,3
     var source = options.source;
-    var tempFilename = 'tmp/'+ new Date().getTime() + '.html';
+    var tempFilename = path.join(__dirname, 'tmp', new Date().getTime() + '.html');
 
     if(typeof engine === 'undefined' || engine ==='') engine = 'htmlcs';
     if(typeof output === 'undefined' || output ==='') output = 'string';
@@ -22,18 +22,20 @@ function evaluate(options) {
     return new Promise(resolve => {
         fs.writeFile(tempFilename, source , function (err,data) {
             if (err) throw err;
+            var config = '--config=' + path.join(__dirname, 'config', 'config.json');
+
             if(engine === 'htmlcs'){
-                var childArgs = ['--config=config/config.json', path.join(__dirname, 'src/HTMLCS_Run.js'), tempFilename, level, errLevel, output];
+                var childArgs = [config, path.join(__dirname, 'src/HTMLCS_Run.js'), tempFilename, level, errLevel, output];
             }
             if(engine === 'axe'){
-                var childArgs = ['--config=config/config.json', path.join(__dirname, 'src/axe_url.js'),  tempFilename, output];
+                var childArgs = [config, path.join(__dirname, 'src/axe_url.js'),  tempFilename, output];
             }
             if(engine === 'chrome'){
-                var childArgs = ['--config=config/config.json', path.join(__dirname, 'src/chrome_url.js'), tempFilename, output]
+                var childArgs = [config, path.join(__dirname, 'src/chrome_url.js'), tempFilename, output]
             }
-            console.log('E N G I N E ' , engine, childArgs);
+            console.log('E N G I N E ' , engine, binPath, childArgs);
 
-            childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+            childProcess.execFile(binPath, childArgs, { cwd: __dirname }, function(err, stdout, stderr) {
                 stdout = stdout.replace('done','');
 
                 resolve(stdout);
